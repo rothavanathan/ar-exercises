43b47cb3f2fbec12f99206b9e4f02dab3b561541
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-04 23:04:11 +0000

    add employees to employees table, create has many relation between stores and employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,49 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Dan",
+  last_name: "Abramov",
+  hourly_rate: 100
+)
+@store1.employees.create(
+  first_name: "Guido",
+  last_name: "von Rossum",
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Brendan",
+  last_name: "Eich",
+  hourly_rate: 60
+)
+@store2.employees.create(
+  first_name: "Eleanor",
+  last_name: "Manskrill",
+  hourly_rate: 60
+)
+@store2.employees.create(
+  first_name: "Felicia",
+  last_name: "Featherbottom",
+  hourly_rate: 60
+)
+@store2.employees.create(
+  first_name: "Gertrude",
+  last_name: "Manskrill",
+  hourly_rate: 60
+)
+@store2.employees.create(
+  first_name: "Betty",
+  last_name: "Porridge",
+  hourly_rate: 100
+)
+@store2.employees.create(
+  first_name: "Mack",
+  last_name: "Power",
+  hourly_rate: 60
+)
+
